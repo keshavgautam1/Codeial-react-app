b58475809d861876987e3de8e131fdca84551fdf
Keshav Gautam <keshavgautam375@gmail.com>
2024-03-19 16:03:06 +0530

    Add PrivateRoute component to protect settings page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import { useAuth } from '../hooks/index.js';
 import { Home, Login, Settings, Signup } from '../pages';
 import { Loader, Navbar } from '../components/index.js';
+
+function PrivateRoute({ children, ...rest }) {
+  const auth = useAuth();
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (auth.user) {
+          return children;
+        }
+        return <Navigate to="/login" />;
+      }}
+    />
+  );
+}
 
 const Page404 = () => {
   return <h1>404</h1>;
@@ -21,7 +41,7 @@ function App() {
           <Route path="/" element={<Home posts={[]} />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Signup />} />
-          <Route path="/settings" element={<Settings />} />
+          <PrivateRoute path="/settings" element={<Settings />} />
           <Route path="*" element={<Page404 />} />
         </Routes>
       </Router>
