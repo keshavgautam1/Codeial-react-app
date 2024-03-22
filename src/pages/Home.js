b6f31e2eb49231060717f18c7a86453c8ae745b1
Keshav Gautam <keshavgautam375@gmail.com>
2024-03-22 21:19:33 +0530

    Add FriendsList component and update Home page layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import styles from '../styles/home.module.css';
 import { UserPic, LikePic, CommentPic } from '../assets/images/index';
-import { Comment, Loader } from '../components';
+import { Comment, Loader, FriendsList } from '../components';
 import { getPosts } from '../api/index.js';
+import { useAuth } from '../hooks/index.js';
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const auth = useAuth();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -26,52 +28,55 @@ const Home = () => {
   }
 
   return (
-    <div className={styles.postsList}>
-      {posts.map((post) => (
-        <div className={styles.postWrapper} key={`post-${post._id}`}>
-          <div className={styles.postHeader}>
-            <div className={styles.postAvatar}>
-              <img src={UserPic} alt="user-pic" />
-              <div>
-                <Link
-                  to={{
-                    pathname: `/user/${post.user._id}`,
-                    state: {
-                      user: post.user,
-                    },
-                  }}
-                  className={styles.postAuthor}
-                >
-                  {post.user.name}
-                </Link>
-                <span className={styles.postTime}>a minute ago</span>
+    <div className={styles.home}>
+      <div className={styles.postsList}>
+        {posts.map((post) => (
+          <div className={styles.postWrapper} key={`post-${post._id}`}>
+            <div className={styles.postHeader}>
+              <div className={styles.postAvatar}>
+                <img src={UserPic} alt="user-pic" />
+                <div>
+                  <Link
+                    to={{
+                      pathname: `/user/${post.user._id}`,
+                      state: {
+                        user: post.user,
+                      },
+                    }}
+                    className={styles.postAuthor}
+                  >
+                    {post.user.name}
+                  </Link>
+                  <span className={styles.postTime}>a minute ago</span>
+                </div>
               </div>
-            </div>
-            <div className={styles.postContent}>{post.content}</div>
+              <div className={styles.postContent}>{post.content}</div>
 
-            <div className={styles.postActions}>
-              <div className={styles.postLike}>
-                <img src={LikePic} alt="likes-icon" />
-                <span>5</span>
+              <div className={styles.postActions}>
+                <div className={styles.postLike}>
+                  <img src={LikePic} alt="likes-icon" />
+                  <span>5</span>
+                </div>
+
+                <div className={styles.postCommentsIcon}>
+                  <img src={CommentPic} alt="comments-icon" />
+                  <span>2</span>
+                </div>
+              </div>
+              <div className={styles.postCommentBox}>
+                <input placeholder="Start typing a comment" />
               </div>
 
-              <div className={styles.postCommentsIcon}>
-                <img src={CommentPic} alt="comments-icon" />
-                <span>2</span>
+              <div className={styles.postCommentsList}>
+                {post.comments.map((comment) => (
+                  <Comment comment={comment} key={`comment-${comment._id}`} />
+                ))}
               </div>
-            </div>
-            <div className={styles.postCommentBox}>
-              <input placeholder="Start typing a comment" />
-            </div>
-
-            <div className={styles.postCommentsList}>
-              {post.comments.map((comment) => (
-                <Comment comment={comment} key={`comment-${comment._id}`} />
-              ))}
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      {auth.user && <FriendsList />}
     </div>
   );
 };
